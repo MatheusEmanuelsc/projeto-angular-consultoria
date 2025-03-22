25d661f63f53e52182491f126077650d89e33afe
matheus emanuelsc <matheus.emanuelsc@gmail.com>
2025-03-22 18:24:45 -0300

    logo

--- src/app/components/logo/logo.component.ts
+++ src/app/components/logo/logo.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-logo',
   imports: [],
+  standalone: true,
   templateUrl: './logo.component.html',
-  styleUrl: './logo.component.css'
+  styleUrl: './logo.component.css',
 })
 export class LogoComponent {
-
+  src = input.required<string>();
+  alt = input.required<string>();
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { LogoComponent } from "../../components/logo/logo.component";
+
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [LogoComponent],
+  standalone: true,
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
-
+  logoSrc = '/assets/logo.png';
+  logoAlt = 'Logotipo da Consultoria';
 }
